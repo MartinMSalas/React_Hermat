adad912758fafc74b1aa251e2d11a1354f7268c7
MartinMSalas <ingmartinmsalas@gmail.com>
2022-12-22 12:47:21 -0300

    Adding files

---
+++ hermat/src/component/App.js
@@ -0,0 +1,16 @@
+
+import '../css/App.css';
+import Example from './Login';
+import MyComponent from './test2';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Example></Example>
+      
+    </div>
+  );
+}
+
+export default App;
